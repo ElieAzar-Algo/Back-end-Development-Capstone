35f3ee6a765581a9faf0829825f2a21d5476cef4
ElieAzar-Algo <elie.azar3891.ea@gmail.com>
2023-08-17 19:09:12 +0300

    implemented django application

--- concert/views.py
+++ concert/views.py
@@ -20,9 +20,10 @@ def signup(request):
         try:
             user = User.objects.filter(username=username).first()
             if user:
-                return render(request, "signup.html", {"form": SignUpForm, "message":"user already exist"})
+                return render(request, "signup.html", {"form": SignUpForm, "message": "user already exist"})
             else:
-                user = User.objects.create(username=username,password=make_password(password))
+                user = User.objects.create(
+                    username=username, password=make_password(password))
                 login(request, user)
                 return HttpResponseRedirect(reverse("index"))
         except User.DoesNotExist:
@@ -53,13 +54,39 @@ def photos(request):
     pass
 
 def login_view(request):
-    pass
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        try:
+            user = User.objects.get(username=username)
+            if user:
+                if user.check_password(password):
+                    login(request, user)
+                    return HttpResponseRedirect(reverse("index"))
+        except User.DoesNotExist:
+            return render(request, "login.html", {"form": LoginForm})
+    return render(request, "login.html", {"form": LoginForm})
 
 def logout_view(request):
-    pass
+    logout(request)
+    return HttpResponseRedirect(reverse("login"))
 
 def concerts(request):
-    pass
+    if request.user.is_authenticated:
+        lst_of_concert = {}
+        concert_objects = Concert.objects.all()
+        for item in concert_objects:
+            try:
+                status = item.attendee.filter(user=request.user).first().attending
+            except:
+                status = "-"
+            lst_of_concert.append({
+                "concert": item,
+                "status": status
+            })
+        return render(request, "concerts.html", {"concerts": lst_of_concert})
+    else:
+        return HttpResponseRedirect(reverse("login"))
 
 
 def concert_detail(request, id):
